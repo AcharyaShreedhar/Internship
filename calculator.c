816c646c2fe6e073d2a8438deae32e51dd212a66
sanjok <sanjokmahat@gmail.com>
2021-02-10 15:34:58 +0545

    mod

--- calculator.c
+++ calculator.c
@@ -1,9 +1,12 @@
 #include<stdio.h>
 #include<conio.h>
 int main(){
-    int a,b;
+    int a,b,mod;
+    a=10,b=4;
     printf("Enter any two numbers a and b");
     scanf("%d,%d",&a,&b);
+    mod=a%b;
+    printf("the remainder is:%d", &mod);
     return 0;
 
 }
