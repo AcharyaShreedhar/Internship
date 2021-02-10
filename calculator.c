1ac3da333b6accbe9b35c9d11c81c271f3cd15a2
anilakheli <kanxoakheli22@gmail.com>
2021-02-10 22:24:50 +0545

    added add function in calculator

--- calculator.c
+++ calculator.c
@@ -1,12 +1,18 @@
-#include<stdio.h>
-#include<conio.h>
-int main(){
-    int a,b,mod;
-    a=10,b=4;
+#include <stdio.h>
+#include <conio.h>
+int main()
+{
+    int a, b, mod, sum;
     printf("Enter any two numbers a and b");
-    scanf("%d,%d",&a,&b);
-    mod=a%b;
+    scanf("%d,%d", &a, &b);
+    mod = a % b;
     printf("the remainder is:%d", &mod);
+    int sum()
+    {
+        int add;
+        add = a + b;
+        return add;
+    }
+    printf("The sum is %d", sum);
     return 0;
-
 }
