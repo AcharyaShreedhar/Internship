19362842a0cd903e5ba7d75467bf24bfd7dd3ae6
aniakheli <chhetry.ani175@gmail.com>
2021-02-10 14:47:11 +0545

    sum

---
+++ sum.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+{
+    int a, b, sum;
+    printf("Enter the two number.\n");
+    scanf("%d%d", &a, &b);
+    printf("The sum is.", sum);
+    return 0;
+}
